3c54f789834deceff08e3f32ddd270441d085310
yglee <yglee2601@gmail.com>
2021-08-10 14:24:32 +0900

    스마트벤드 > 스마트벤드이름 변경

--- htdocs/assets/js/utility.js
+++ htdocs/assets/js/utility.js
@@ -1,7 +1,17 @@
 // Utility Class & Function Here
 
 function refreshData(seconds) {
-    setTimeout(function () {
-        location.reload();
-    }, seconds * 1000);
+  setTimeout(function () {
+    location.reload();
+  }, seconds * 1000);
+}
+
+function makeDateYYYYMMDDHHMM(timeStamp) {
+  const year = timeStamp.getFullYear() + '.';
+  const month = (timeStamp.getMonth() + 1 + '').padStart(2, '0') + '.';
+  const date = (timeStamp.getDate() + '').padStart(2, '0') + ' ';
+  const hours = (timeStamp.getHours() + '').padStart(2, '0') + ':';
+  const mins = (timeStamp.getMinutes() + '').padStart(2, '0');
+  const YYYYMMDDHHMM = year + month + date + hours + mins;
+  return YYYYMMDDHHMM;
 }
